060e7d93f7176698ea92b5180bd80f126bdeab48
Powerisinschool <toolulopeolagunju@gmail.com>
2023-02-27 15:57:58 +0100

    Task 2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+		return (NULL);
+
+	binary_tree_t *node = binary_tree_node(parent, value);
+
+	if (node == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		node->right = parent->right;
+		parent->right->parent = node;
+	}
+
+	parent->right = node;
+
+	return (node);
+}
