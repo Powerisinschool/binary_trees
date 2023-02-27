b2b96f5c5dd553bad296040ea890dbcc11e1de36
Powerisinschool <toolulopeolagunju@gmail.com>
2023-02-27 10:28:33 +0100

    Task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+		return (NULL);
+
+	binary_tree_t *node = binary_tree_node(parent, value);
+
+	if (node == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		node->left = parent->left;
+		parent->left->parent = node;
+	}
+
+	parent->left = node;
+
+	return (node);
+}
