c6c302a6cb393741fad356eb6a58c13534077971
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2023-02-27 16:48:16 +0100

    Update 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+void delete_node(binary_tree_t *node);
+
 void binary_tree_delete(binary_tree_t *tree)
 {
 	binary_tree_t *tmp;
@@ -8,5 +10,16 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree == NULL)
 		return;
 
-	delete_right(tree);
+	delete_node(tree->left);
+	delete_node(tree->right);
+	free(tree);
+}
+
+void delete_node(binary_tree_t *node)
+{
+    if (node == NULL)
+        return;
+    delete_node(node->left);
+    delete_node(node->right);
+    free(node);
 }
