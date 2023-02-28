fc06d416f3df893292be3e4b7da9d7d1ff4a6d96
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2023-02-28 14:38:15 +0100

    Task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,41 @@
+#include "binary_trees.h"
+
+/**
+ * height - Subfunction used to avoid top level addition
+ * else top function would be enough :)
+ * 
+ * @node: Node
+ * 
+ * Return: 1 if node
+*/
+size_t height(const binary_tree_t *node);
+
+/**
+ * binary_tree_height - Find the height of a binary tree
+ * 
+ * @tree: Pointer to the root node of tree
+ * 
+ * Return: Height of tree (size_t)
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (height(tree->left) > height(tree->right))
+		return (height(tree->left));
+	else
+		return (height(tree->right));
+}
+
+size_t height(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+
+	if (height(node->left) > height(node->right))
+		return (height(node->left) + 1);
+	else
+		return (height(node->right) + 1);
+
+}
