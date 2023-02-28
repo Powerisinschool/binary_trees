0b2d4453a559785914f3ba79a75b07afa3b8ecbe
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2023-02-28 13:56:28 +0100

    No need for complication

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -8,24 +8,10 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	binary_tree_t *node;
 	if (!tree || !func)
 		return;
 
-	node = (binary_tree_t *) tree;
-
-	/* Get to the left end of the binary tree */
-	while (node->left != NULL)
-		node = node->left;
-
-	while (node->parent != tree->parent)
-	{
-		func(node->n);
-		binary_tree_inorder((const binary_tree_t *) node->right, func);
-		node = node->parent;
-	}
-
-	func(node->n);
-	binary_tree_inorder((const binary_tree_t *) node->right, func);
-	node = node->parent;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
