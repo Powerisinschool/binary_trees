8e7735ba24970d6c0ba895e1df4992a1c07574bd
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2023-02-28 13:08:20 +0100

    Task 2 Fix and betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,11 +1,19 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_right - Creates a new node to the right of a binary tree
+ *
+ * @parent: binary_tree_t * (pointer to the top node of the tree)
+ * @value: int
+ *
+ * Return: pointer to the new node created
+ */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *node = binary_tree_node(parent, value);
+
 	if (parent == NULL)
 		return (NULL);
-
-	binary_tree_t *node = binary_tree_node(parent, value);
 
 	if (node == NULL)
 		return (NULL);
