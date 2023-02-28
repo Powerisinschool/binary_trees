e4a027fa6d65695b141b595d72e74884f03231c8
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2023-02-28 13:33:13 +0100

    Task 2 & 4 fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,12 +10,14 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = binary_tree_node(parent, value);
+	binary_tree_t *node;
 
-	if (parent == NULL)
+	if (!parent)
 		return (NULL);
 
-	if (node == NULL)
+	node = binary_tree_node(parent, value);
+
+	if (!node)
 		return (NULL);
 
 	if (parent->right != NULL)
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_leaf - Checks if a tree node is a leaf
+ *
+ * @node: binary_tree_t * (pointer to the node)
+ *
+ * Return: 1 if node is a leaf, otherwise 0
+ */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
