180983cecb5236b2e84121796b0929c41d62614a
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2023-02-28 15:20:50 +0100

    Task 12 betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,10 +9,10 @@
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    if (!(tree->left) && !(tree->right))
-        return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right) + 1);
-    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	if (!(tree->left) && !(tree->right))
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right) + 1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
