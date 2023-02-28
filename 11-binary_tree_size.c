90d856fe4eaf37b409658caddd9a4db90382f0e5
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2023-02-28 15:05:35 +0100

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,6 +1,12 @@
 #include "binary_trees.h"
 
-/***/
+/**
+ * binary_tree_size - Find the size of a binary tree from node
+ *
+ * @tree: Pointer to node
+ *
+ * Return: Size of tree
+*/
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
